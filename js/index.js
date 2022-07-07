eb97a8ec4ff5ab397e313c489175569f6ff8390f
Pedro Seneriz <pedroseneriz01@gmail.com>
2022-07-07 15:51:16 -0300

    agregadas algunas cosas

--- js/index.js
+++ js/index.js
@@ -1,26 +1,37 @@
-const sheetsWrite = document.querySelectorAll(".sheet-write"); sheetsWrite.forEach((e => { const t = e.dataset; let l = JSON.parse(t.words), i = l.reduce(((e, t) => (t && "" != t.trim() && e.push(t), e)), []), n = i.filter(((e, t) => { let n = []; if (t >= 0) { if (!(i.length > 1)) return n.push(e), n; if (l[t - 1] != l[t] && e) return n.push(e), n } })), s = n.length; if (!n) return; let r = document.createElement("span"); r.classList.add("blink-cursor"), r.textContent = "|", e.parentElement.appendChild(r); let a = t.initdelay ? t.initdelay : 10, d = t.writespeed ? t.writespeed : 100, c = t.deletespeed ? t.deletespeed : 40, o = t.delaydelete ? t.delaydelete : 5e3, p = !t.infinite, u = !0, f = !0, h = 0, m = 0, v = 0; setInterval((function () { if (u && m++, m >= a * v) { u = !1; let t = n[v].split(""), l = t.length, i = 0, a = setInterval((function () { i >= h && (e.innerHTML += t[i]), i++, i >= l && (r.classList.add("stop-write"), (v >= s && !p || s <= 1) && (f = !1), f && setTimeout((function () { r.classList.remove("stop-write"); let t = setInterval((function () { let l = "", r = ""; v >= s && (l = n[v - 1].split(""), r = n[0].split("")), v > 0 && v < s && (l = n[v].split(""), r = n[v - 1].split("")), h = 0; let a = 0; for (; r[a] == l[a];)h++, a++; i >= h && (e.innerHTML = n[v - 1].slice(0, i)), i--, i < 0 && (v >= s ? setTimeout((function () { v = 0, m = 0, u = !0 }), d) : u = !0, clearInterval(t)) }), c) }), o), clearInterval(a)) }), d); v++ } })) }));
+//const sheetsWrite = document.querySelectorAll(".sheet-write"); sheetsWrite.forEach((e => { const t = e.dataset; let l = JSON.parse(t.words), i = l.reduce(((e, t) => (t && "" != t.trim() && e.push(t), e)), []), n = i.filter(((e, t) => { let n = []; if (t >= 0) { if (!(i.length > 1)) return n.push(e), n; if (l[t - 1] != l[t] && e) return n.push(e), n } })), s = n.length; if (!n) return; let r = document.createElement("span"); r.classList.add("blink-cursor"), r.textContent = "|", e.parentElement.appendChild(r); let a = t.initdelay ? t.initdelay : 10, d = t.writespeed ? t.writespeed : 100, c = t.deletespeed ? t.deletespeed : 40, o = t.delaydelete ? t.delaydelete : 5e3, p = !t.infinite, u = !0, f = !0, h = 0, m = 0, v = 0; setInterval((function () { if (u && m++, m >= a * v) { u = !1; let t = n[v].split(""), l = t.length, i = 0, a = setInterval((function () { i >= h && (e.innerHTML += t[i]), i++, i >= l && (r.classList.add("stop-write"), (v >= s && !p || s <= 1) && (f = !1), f && setTimeout((function () { r.classList.remove("stop-write"); let t = setInterval((function () { let l = "", r = ""; v >= s && (l = n[v - 1].split(""), r = n[0].split("")), v > 0 && v < s && (l = n[v].split(""), r = n[v - 1].split("")), h = 0; let a = 0; for (; r[a] == l[a];)h++, a++; i >= h && (e.innerHTML = n[v - 1].slice(0, i)), i--, i < 0 && (v >= s ? setTimeout((function () { v = 0, m = 0, u = !0 }), d) : u = !0, clearInterval(t)) }), c) }), o), clearInterval(a)) }), d); v++ } })) }));
 
 
 
-let pepe;
+let scrollY;
 
 window.onscroll = function () {
 
-    let nav = document.querySelector(".navb");// document.getElementById("navb");
+    let elemento = document.querySelectorAll(".list li");
 
-    pepe = window.scrollY;
-    console.log("Y" + window.scrollY);
-    if (pepe > 0) {
-        nav.style.backgroundColor = "red";
-        console.log("COLOR OSCURO");
+    scrollY = window.scrollY;
+
+    if (scrollY > 2362) {
+        setColor(elemento, 3);
     }
-    else {
-        nav.style.backgroundColor = "black";
-        console.log("COLOR CLARO");
+    else if (scrollY > 1361) {
+        setColor(elemento, 2);
+    }
+    else if (scrollY > 982) {
+        setColor(elemento, 1);
+    }
+    else if (scrollY > 457) {
+        setColor(elemento, 0);
     }
 };
 
-/*************************MAQUINA DE ESCRIBIR********************************* */
+function setColor(element, position) {
+    element.forEach(el => {
+        el.classList.remove("onView")
+    });
+    element[position].classList.add("onView")
+}
+
+//#region MAQUINA DE ESCRIBIR
 let timer;
 let i = 0;
 
@@ -66,5 +77,6 @@ window.onload = function () {
     }
 
     typeWriter();
-
 }
+
+//#endregion
